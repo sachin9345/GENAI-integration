a572e5e0d30f9b30316d37d66b471b10724ab3ce
sachin9345 <135344828+sachin9345@users.noreply.github.com>
2025-01-11 17:33:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const genAI = new GoogleGenerativeAI("AIzaSyAGHRC1sbzUo6pH87Z7y4c5dCtJW6HlzNQ");
+const genAI = new GoogleGenerativeAI("");
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 async function run() {
